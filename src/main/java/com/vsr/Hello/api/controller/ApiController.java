89d9ba77742067f51060303427fd891a5d674dea
Sreedhar47 <vemana.java.com>
2024-11-13 16:49:18 +0530

    added

---
+++ src/main/java/com/vsr/Hello/api/controller/ApiController.java
@@ -0,0 +1,17 @@
+package com.vsr.Hello.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+	
+	@GetMapping("/get")
+	public ResponseEntity<String> welocme(){
+		return new ResponseEntity<>("WELCOME TO HELLO API", HttpStatus.OK);
+	}
+	
+}
