4cc2eadd334a170b99c2f6bf438a4d448f598713
Sreedhar47 <vemana.java.com>
2024-11-25 18:56:01 +0530

    updated

--- src/main/java/com/vsr/Hello/api/HelloApiApplication.java
+++ src/main/java/com/vsr/Hello/api/HelloApiApplication.java
@@ -1,4 +1,4 @@
-package com.vsr.Hello.api;
+package com.vsr.hello.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/vsr/Hello/api/controller/ApiController.java
+++ src/main/java/com/vsr/Hello/api/controller/ApiController.java
@@ -1,4 +1,4 @@
-package com.vsr.Hello.api.controller;
+package com.vsr.hello.api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
