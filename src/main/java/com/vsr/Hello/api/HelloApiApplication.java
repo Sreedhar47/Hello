bf6ef035f77209edb739d31b623197d0f968cd8b
Sreedhar47 <vemana.java.com>
2024-11-26 12:43:04 +0530

    added Junit

--- src/main/java/com/vsr/Hello/api/HelloApiApplication.java
+++ src/main/java/com/vsr/Hello/api/HelloApiApplication.java
@@ -3,7 +3,11 @@ package com.vsr.Hello.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "HELLO API", version = "1.0", description = "API for managing users"))
 public class HelloApiApplication {
 
 	public static void main(String[] args) {
